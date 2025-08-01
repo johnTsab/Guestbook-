802e1ed11f02948932e20d6ee318c31bec7a3a0f
johnTsab <187337489+johnTsab@users.noreply.github.com>
2025-08-01 13:59:31 +0300

    second commit for frontend

--- guestbook/src/app/app.routes.ts
+++ guestbook/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './guest/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"guests/home" , component:HomeComponent},
+    {path:"guests",redirectTo:"guests/home",pathMatch:"full"},
+    {path:"",redirectTo:"guests/home",pathMatch:"full"}
+];
--- guestbook/src/app/guest/guest.ts
+++ guestbook/src/app/guest/guest.ts
@@ -1,2 +1,6 @@
 export interface Guest {
+    id:Number,
+    name:String,
+    email:String,
+    address:String
 }
