f3d82388eba12a39e6bea0f92be8a3fa85270e75
johnTsab <187337489+johnTsab@users.noreply.github.com>
2025-08-02 14:07:14 +0300

    Link to database , implemented search function , implemented light/dark mode and angular material table

--- guestbook/src/app/guest/guest-serv.service.ts
+++ guestbook/src/app/guest/guest-serv.service.ts
@@ -1,9 +1,17 @@
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Guest} from './guest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuestServService {
 
-  constructor() { }
+  constructor(private _httpClient:HttpClient) { }
+
+  baseUrl : String = "/api/v1/guests";
+  fetchAllGuests():Observable<Guest[]>{
+    return this._httpClient.get<Guest[]>(`${this.baseUrl}`);
+  }
 }
--- guestbook/src/app/guest/home/home.component.ts
+++ guestbook/src/app/guest/home/home.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableDataSource,MatTableModule} from '@angular/material/table';
+import {Guest} from '../guest';
+import { GuestServService } from '../guest-serv.service';
+import { ViewEncapsulation } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,MatIconModule,MatButtonModule,MatTableModule,MatSlideToggleModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  encapsulation:ViewEncapsulation.None
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'email', 'address'];
+  dataSource = new MatTableDataSource<Guest>();
+  guest:Guest = {
+    id:0,
+    name:'',
+    email:'',
+    address:''
+  }
+  guests: Guest[]=[];
+  isDarkMode = false;
+ toggleDarkMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
 
+  
+  
+  
+  
+  constructor(private guestService:GuestServService){}
+  ngAfterViewInit(): void {
+      this.guestService.fetchAllGuests().subscribe((data)=>{
+        this.guests = data;
+        this.dataSource = new MatTableDataSource<Guest>(data);
+      })
+  }
+
+  filteredGuests : Guest[] = [];
+  searchGuest(input:String){
+    this.filteredGuests = this.guests.filter(item=>item.name.toLowerCase().includes(input.toLowerCase())
+    || item.email.toLowerCase().includes(input.toLowerCase()) || item.address.toLowerCase().includes(input.toLowerCase()))
+    this.dataSource = new MatTableDataSource<Guest>(this.filteredGuests);
+  }
 }
